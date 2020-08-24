a26f899b03f398a93df55b0d788a4d11e5df6252
Yohanes R F Silitonga <60679808+yohanesray21@users.noreply.github.com>
2020-08-24 14:08:02 +0700

    Add files via upload

---
+++ assets/storage.js
@@ -0,0 +1,60 @@
+//Variabel ini digunakan sebagai key untuk mengakses dan 
+//menyimpan data pada localStorage.
+
+const CACHE_KEY = "calculation_history";
+
+//mengembalikan nilai boolean dari pengecekan fitur Storage pada browser.
+
+function checkForStorage() {
+    return typeof(Storage) !== "undefined"
+}
+
+//funsi untuk menyimpan data riwayat kalkulasi pada localStorage
+
+function putHistory(data) {
+    if(checkForStorage()) {
+        let historyData = null;
+        if(localStorage.getItem(CACHE_KEY) === null) {
+            historyData = [];
+        } else {
+            historyData = JSON.parse(localStorage.getItem(CACHE_KEY));
+        }
+
+        historyData.unshift(data);
+
+        if(historyData.length > 5) {
+            historyData.pop;
+        }
+
+        localStorage.setItem(CACHE_KEY, JSON.stringify(historyData))
+    }
+}
+
+function showHistory() {
+    if(checkForStorage()) {
+        return JSON.parse(localStorage.getItem(CACHE_KEY)) || [];
+    } else {
+        return [];
+    }
+}
+
+function renderHistory() {
+    const historyData = showHistory();
+    let historyList = document.querySelector("#historyList");
+
+    // selalu hapus konten HTML pada elemen historyList agar tidak menampilkan data ganda
+    
+    historyList.innerHTML = "";
+
+    for(let history of historyData) {
+        let row = document.createElement('tr');
+        row.innerHTML = "<td>" + history.firstNumber + "</td>"
+        row.innerHTML += "<td>" + history.operator + "</td>";
+        row.innerHTML += "<td>" + history.secondNumber + "</td>";
+        row.innerHTML += "<td>" + history.result + "</td>";
+        
+        historyList.appendChild(row);
+    }
+}
+
+renderHistory();
